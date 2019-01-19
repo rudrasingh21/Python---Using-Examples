7f984415a7b78171d22f13f2798d7b9e91f5a7fb
rudrasingh21 <30667041+rudrasingh21@users.noreply.github.com>
2019-01-20 01:41:21 +0530

    Update #Print Pattern.py

--- #Print Pattern.py
+++ #Print Pattern.py
@@ -24,3 +24,26 @@ print("1",sep=" ",end=" ")
 print("2",sep=" ",end=" \n")
 
 print( 3* ' ' + 5 * '*')
+
+
+
+*******************************************************
+#Using Command Prompt - Output
+
+>>> print('G','F','G', sep='')
+GFG
+>>> print('G','F','G', sep=' ')
+G F G
+>>> print('G','F','G', sep='-')
+G-F-G
+>>> print('G','F','G', sep=' ',end=' ')
+G F G >>> print('09','12','2016', sep='-')
+09-12-2016
+>>> print('pratik','geeksforgeeks', sep='@')
+pratik@geeksforgeeks
+>>> print("1",sep=" ",end=" ")
+1 >>> print("2",sep=" ",end=" \n")
+2
+>>> print( 3* ' ' + 5 * '*')
+   *****
+>>>
